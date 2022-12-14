250cbba5892bb2242782e531bf3787e4e3f17148
Lucas Vallejo <lucasvallejo92@gmail.com>
2022-12-13 21:57:44 -0300

    🔖 change app name

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,6 @@ export default function Home({ posts }) {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
         <div className={styles.grid}>
           {
             posts.map(({ data }, key) => (
